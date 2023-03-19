09d6f3c2e2eff681df0cf7659651f0c000dee8ec
palisthadeshar <67106733+palisthadeshar@users.noreply.github.com>
2023-03-20 00:09:25 +0545

    Update MainActivity.kt

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,38 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
-
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+   
     }
 
-    private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        val dice2 = Dice(6)
-        val diceRoll2 = dice2.roll()
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll2.toString()
+   
 
 
-    }
-
-
-}
-/**
- * Roll the dice and update the screen with the result.
- */
-class Dice(private val numSides: Int) {
-
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
-}
